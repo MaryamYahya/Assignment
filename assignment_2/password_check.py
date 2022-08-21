5284f6605dd087ed75f232d0f51921a42a1032d3
Maryam_Yahya <111171905+MaryamYahya@users.noreply.github.com>
2022-08-21 12:35:04 +0430

    Update password_check.py

--- assignment_2/password_check.py
+++ assignment_2/password_check.py
@@ -2,8 +2,8 @@ username="ali"
 password=1234
 tried_c=0
 for i in range(3):
-    your_username=input("please inter the username: ")
-    your_password=int(input("please inter the password: "))
+    your_username=input("please enter the username: ")
+    your_password=int(input("please enter the password: "))
     if (your_username==username) and (your_password==password):
         print("welcome")
         break
